3f889ba2efede9a503ea5432a1491d59312b3414
Gustavo <gustavo.fior@outlook.com>
2023-06-04 22:35:18 -0300

    fixing web app

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -2,8 +2,17 @@ import type { NextPage } from "next";
 import { Nunito_Sans } from "next/font/google";
 import Head from "next/head";
 import Image from "next/image";
+import {
+  FaAndroid,
+  FaApple,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
 
+import Abdul from "../../assets/abdul.jpeg";
 import AppIcon from "../../assets/app_icon.png";
+import Gustavo from "../../assets/gustavo2.jpg";
 
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
@@ -19,73 +28,90 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
-        className={`${nunitoSans.variable} flex h-screen  flex-col items-center justify-center gap-8 bg-blue-400 font-nunito-sans text-white`}
+        className={`${nunitoSans.variable} flex h-screen flex-col items-center justify-center gap-8 bg-gradient-to-r from-[#202356] to-[#032b50] font-nunito-sans text-white`}
       >
-        <div className="flex gap-2">
+        <div className="flex flex-col gap-4">
           <div className="flex items-start justify-center">
             <Image src={AppIcon} width={64} height={64} alt="Mind app icon" />
           </div>
           <div>
-            <h1 className="text-4xl">Mind</h1>
-            <h2 className="text-lg">
-              Schedule and payment management <br /> for therapy sessions
-            </h2>
-            <p className="text-sm">Coming soon to Android and iOS</p>
+            <h1 className="text-4xl font-bold">Mind</h1>
           </div>
         </div>
+        <h2 className="text-center text-lg">
+          Schedule and payment management <br /> for therapy sessions
+        </h2>
+        <p className="text-sm text-slate-400">
+          Coming soon to {<FaAndroid className="m-1 inline-block" />} and{" "}
+          {<FaApple className="m-1 inline-block" />}
+        </p>
 
         <div className="flex w-full max-w-lg flex-col items-center justify-center gap-2">
           <h3 className="text-2xl">Meet the founders</h3>
-          <div className="flex w-full justify-around">
-            <div>
+          <div className="flex w-full justify-center gap-12 pt-8">
+            <div className="flex flex-col items-center gap-2">
+              <Image
+                className="rounded-full"
+                width={64}
+                height={64}
+                src={Gustavo}
+                alt="Gustavo Fior"
+              />
               <p className="text-center text-lg font-bold">Gustavo Fior</p>
-              <a
-                target="_blank"
-                className="text-center"
-                href="mailto:gustavo@getmind.app"
-              >
-                gustavo@getmind.app
-              </a>
-              <div className="flex justify-between">
+              <div className="flex justify-between gap-3">
                 <a
                   target="_blank"
-                  className="text-slate-300-500"
-                  href="https://www.linkedin.com/in/gustavo-fior-a910781b4/"
+                  className="text-center transition duration-300 ease-in-out hover:text-slate-300"
+                  href="mailto:gustavo@getmind.app"
                 >
-                  LinkedIn
+                  {<FaEnvelope />}
                 </a>
                 <a
                   target="_blank"
-                  className="text-slate-300-500"
+                  className="text-center transition duration-300 ease-in-out hover:text-slate-300"
+                  href="https://www.linkedin.com/in/gustavo-fior-a910781b4/"
+                >
+                  {<FaLinkedin />}
+                </a>
+                <a
+                  target="_blank"
+                  className="text-center transition duration-300 ease-in-out hover:text-slate-300"
                   href="https://github.com/gustavo-fior"
                 >
-                  GitHub
+                  {<FaGithub />}
                 </a>
               </div>
             </div>
-            <div>
+            <div className="flex flex-col items-center gap-2">
+              <Image
+                className="rounded-full"
+                width={64}
+                height={64}
+                src={Abdul}
+                alt="Abdul Haidar"
+              />
               <p className="text-center text-lg font-bold">Abdul Haidar</p>
-              <a
-                target="_blank"
-                href="mailto:abdul@getmind.app"
-                className="text-center"
-              >
-                abdul@getmind.app
-              </a>
-              <div className="flex justify-between">
+              <div className="flex justify-between gap-3">
                 <a
                   target="_blank"
-                  className="text-slate-300-500"
-                  href="https://www.linkedin.com/in/abdulhdr/"
+                  className="text-center transition duration-300 ease-in-out hover:text-slate-300"
+                  href="mailto:abdul@getmind.app"
                 >
-                  LinkedIn
+                  {<FaEnvelope />}
                 </a>
                 <a
                   target="_blank"
-                  className="text-slate-300-500"
+                  className="text-center transition duration-300 ease-in-out hover:text-slate-300"
+                  href="https://www.linkedin.com/in/abdulhdr/"
+                >
+                  {<FaLinkedin />}
+                </a>
+                <a
+                  target="_blank"
+                  className="text-center transition duration-300 ease-in-out hover:text-slate-300"
                   href="https://github.com/abdulhdr1"
                 >
-                  GitHub
+                  {<FaGithub />}
                 </a>
               </div>
             </div>
