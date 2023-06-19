34a4a6a07dfb2d2401d20a72911204919cd83e0b
Abdul Haidar <abdul.haidar@bancosb.com.br>
2023-06-19 15:15:02 -0300

    try migration to cloudflare pages

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 
 import { appRouter, createTRPCContext } from "@acme/api";
 
+export const runtime = "edge";
+
 const essentialEnv = z.object({
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
