528b364a2d2224c764763196a80f777e9c9b112c
Gustavo <gustavo.fior@outlook.com>
2023-07-18 23:16:38 -0300

    increase mockup resolution and add logo

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -7,7 +7,8 @@ import { BsArrowRightShort } from "react-icons/bs";
 
 import AppStore from "../../assets/app_store.png";
 import PlayStore from "../../assets/google_play.png";
-import Mockup from "../../assets/mockup.png";
+import Icon from "../../assets/icon.png";
+import Mockup from "../../assets/mock-hd.png";
 
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
@@ -22,11 +23,19 @@ const Home: NextPage = () => {
         <title>Mind</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main
         className={`${nunitoSans.variable} flex h-screen flex-col items-center justify-center bg-off-white px-32 align-middle font-nunito-sans text-black`}
       >
         <div className="flex items-center justify-center gap-24 align-middle">
-          <div className="mb-4 flex flex-col gap-4">
+          <div className="mb-4 flex flex-col gap-6">
+            <Image
+              className="mb-6"
+              width={48}
+              height={48}
+              src={Icon}
+              alt="Mind logo"
+            />
             <h2 className="text-5xl font-bold">
               Therapy made easy, <br />{" "}
               <span className="text-blue-500">for everyone</span>
@@ -35,20 +44,19 @@ const Home: NextPage = () => {
               Mind helps you to find the best therapist, schedule
               <br /> appointments and pay for them.
             </p>
-            <div className="flex items-center gap-4 align-middle">
+            <div className="flex items-center gap-8 align-middle">
               <Link href="https://drive.google.com/file/d/17eLDNSTf1d6JOi_rno87TqVCkSjuFAf4/view?usp=drive_link">
                 <button className="rounded-lg bg-blue-500 px-4 py-2 font-bold text-white transition duration-300 hover:bg-blue-400">
                   Quick demo
                 </button>
               </Link>
-              <Link
-                href="mailto:contact@getmind.app"
-                className="transition duration-300 hover:text-slate-600"
-              >
-                Talk to us
-                <div className="group rounded-full bg-white bg-opacity-20 px-4 py-1.5 drop-shadow-lg backdrop-blur-lg duration-200 hover:bg-opacity-40">
-                  <div className="flex items-center transition-transform duration-500 group-hover:translate-x-1">
-                    <BsArrowRightShort size={24} />
+              <Link href="mailto:contact@getmind.app">
+                <div className="group duration-200">
+                  <div className="flex items-center gap-1.5">
+                    <p>Talk to us</p>
+                    <div className="flex items-center transition-transform duration-500 group-hover:translate-x-1">
+                      <BsArrowRightShort size={24} />
+                    </div>
                   </div>
                 </div>
               </Link>
@@ -64,7 +72,7 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div>
-          <p className="text-center text-lg font-bold">Soon on</p>
+          <p className="pb-2 text-center text-lg">Soon on</p>
           <div className="flex items-center gap-2">
             <div>
               <Image width={120} height={120} src={AppStore} alt="App store" />
