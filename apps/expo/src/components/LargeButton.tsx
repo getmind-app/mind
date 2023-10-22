91f54a3b86deacc008e69e920926be0a39bbf0ad
Abdul Haidar <abdulhdr1@gmail.com>
2023-10-22 16:05:00 -0300

    add LargeButton component

---
+++ apps/expo/src/components/LargeButton.tsx
@@ -0,0 +1,48 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { Loading } from "./Loading";
+
+type TouchableOpacityProps = React.ComponentProps<typeof TouchableOpacity>;
+
+export function LargeButton({
+    children,
+    ...props
+}: TouchableOpacityProps & {
+    loading?: boolean;
+    children: React.ReactNode;
+}) {
+    return (
+        <TouchableOpacity
+            style={{
+                width: "100%",
+                backgroundColor:
+                    props.disabled || props.loading ? "#bfdbfe" : "#3b82f6",
+                borderRadius: 8,
+                paddingVertical: 8,
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+            {...props}
+        >
+            <View
+                style={{
+                    position: "relative",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Loading
+                    style={{
+                        position: "absolute",
+                        left: -24,
+                    }}
+                />
+                <Text
+                    className={`relative font-nunito-sans-bold text-lg text-white`}
+                >
+                    {children}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
