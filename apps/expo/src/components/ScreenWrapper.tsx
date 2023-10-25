338821ee7db3d8ffc67e6630c05f794326b0e97f
Abdul Haidar <abdulhdr1@gmail.com>
2023-10-24 22:52:25 -0300

    refactor ScreenWrapper to use SafeAreaView

--- apps/expo/src/components/ScreenWrapper.tsx
+++ apps/expo/src/components/ScreenWrapper.tsx
@@ -1,9 +1,27 @@
-import { View } from "react-native";
+import { SafeAreaView, View } from "react-native";
 
 export const ScreenWrapper = ({
     children,
 }: {
     children: React.ReactNode;
 }): JSX.Element => {
-    return <View className="h-full bg-off-white px-4 pt-24">{children}</View>;
+    return (
+        <SafeAreaView
+            style={{
+                flex: 1,
+                backgroundColor: "#f8f8f8",
+            }}
+        >
+            <View
+                style={{
+                    flex: 1,
+                    padding: 30,
+                    paddingHorizontal: 16,
+                    paddingTop: 48,
+                }}
+            >
+                {children}
+            </View>
+        </SafeAreaView>
+    );
 };
