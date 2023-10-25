ab9b5909de357a63e7c56a6f43951978bf5b2111
Abdul Haidar <abdulhdr1@gmail.com>
2023-10-24 22:53:58 -0300

    refactor onboard

--- apps/expo/src/app/onboard.tsx
+++ apps/expo/src/app/onboard.tsx
@@ -18,6 +18,8 @@ import { requestTrackingPermissionsAsync } from "expo-tracking-transparency";
 import { useUser } from "@clerk/clerk-expo";
 import { Trans } from "@lingui/macro";
 
+import { LargeButton } from "../components/LargeButton";
+import { ScreenWrapper } from "../components/ScreenWrapper";
 import { useUserMutations } from "../hooks/user/useUserMutations";
 
 export default function Onboard() {
@@ -34,8 +36,10 @@ export default function Onboard() {
     >(false);
     const notificationListener = useRef<Notifications.Subscription>();
     const responseListener = useRef<Notifications.Subscription>();
+    const [loading, setLoading] = useState(false);
 
     async function handleNext() {
+        setLoading(true);
         if (selectedRole) {
             try {
                 await setMetadata.mutateAsync({
@@ -54,6 +58,8 @@ export default function Onboard() {
             } catch (e) {
                 console.error("error setting metadata");
                 console.error(e);
+            } finally {
+                setLoading(false);
             }
         } else {
             throw new Error("No role selected");
@@ -61,8 +67,6 @@ export default function Onboard() {
     }
 
     useEffect(() => {
-        async () => {};
-
         (async () => {
             await requestTrackingPermissionsAsync();
 
@@ -104,104 +108,159 @@ export default function Onboard() {
     }, []);
 
     return (
-        <SafeAreaView className="flex-1 flex-wrap items-center justify-center bg-off-white">
-            <View className="flex w-full gap-y-4 px-4">
-                <Text className="mb-2 px-4 font-nunito-sans text-3xl">
-                    <Trans>Who are you?</Trans>
-                </Text>
-                <View className="items-center">
-                    <Pressable
-                        onPress={() => setSelectedRole("patient")}
-                        className={`w-full ${
-                            setMetadata.isLoading ? "opacity-30" : ""
-                        } relative overflow-hidden`}
+        <ScreenWrapper>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "space-between",
+                }}
+            >
+                <View
+                    style={{
+                        flex: 1,
+                        rowGap: 16,
+                    }}
+                >
+                    <Text className="font-nunito-sans text-3xl">
+                        <Trans>Who are you?</Trans>
+                    </Text>
+                    <View
+                        style={{
+                            flex: 1,
+                            justifyContent: "center",
+                            rowGap: 16,
+                        }}
                     >
                         <View
-                            className={`flex w-full flex-row rounded-xl border-2 bg-white ${
-                                selectedRole === "patient"
-                                    ? "border-blue-500"
-                                    : "border-white"
-                            }`}
+                            style={{
+                                flex: 1,
+                                maxHeight: "30%",
+                            }}
                         >
-                            <View className="items-center">
-                                <View className="gap-y-2 px-4 py-10">
-                                    <Text className="ml-4 font-nunito-sans text-2xl">
-                                        <Trans>Patient</Trans>
-                                    </Text>
-                                    <Text className="ml-4 font-nunito-sans text-lg text-slate-500">
-                                        <Trans>Looking for help or</Trans>
-                                        {"\n"}
-                                        <Trans>just want to talk</Trans>
-                                    </Text>
-                                </View>
-                            </View>
-                            <Image
-                                alt="Patient with a plant"
-                                source={require("../../assets/images/girl_flower.png")}
-                                className="absolute -bottom-2 right-4 h-36 w-36"
-                                resizeMode="contain"
-                            />
-                        </View>
-                    </Pressable>
-                </View>
-                <View className="items-center">
-                    <Pressable
-                        onPress={() => setSelectedRole("professional")}
-                        className={`w-full ${
-                            setMetadata.isLoading ? "opacity-30" : ""
-                        } relative overflow-hidden`}
-                    >
-                        <View
-                            className={`flex w-full flex-row justify-end rounded-xl border-2 bg-white ${
-                                selectedRole === "professional"
-                                    ? "border-blue-500"
-                                    : "border-white"
-                            }`}
-                        >
-                            <Image
-                                alt="Psychologist reading a book"
-                                source={require("../../assets/images/girl_book.png")}
-                                className="absolute -bottom-2 left-6 h-36 w-36"
-                                resizeMode="contain"
-                            />
-                            <View className="w-3/4 items-center">
-                                <View className="ml-12 gap-y-2 px-4 py-10">
-                                    <Text className="font-nunito-sans text-2xl">
-                                        <Trans>Professional</Trans>
-                                    </Text>
-                                    <Text className="font-nunito-sans text-lg text-slate-500">
-                                        <Trans>Meet and help</Trans>
-                                        {"\n"}
-                                        <Trans>new patients</Trans>
-                                    </Text>
-                                </View>
-                            </View>
-                        </View>
-                    </Pressable>
-                    <TouchableOpacity
-                        className="w-full"
-                        disabled={!selectedRole}
-                        onPress={handleNext}
-                    >
-                        <View
-                            className={` mt-12 flex w-full items-center  justify-center rounded-xl py-2 ${
-                                selectedRole
-                                    ? "bg-blue-500"
-                                    : "bg-gray-300 opacity-50"
-                            }`}
-                        >
-                            <Text
-                                className={`font-nunito-sans-bold text-lg ${
-                                    selectedRole ? "text-white" : "text-black"
-                                }`}
+                            <Pressable
+                                onPress={() => setSelectedRole("patient")}
+                                style={{
+                                    flex: 1,
+                                    opacity: setMetadata.isLoading ? 0.3 : 1,
+                                    borderWidth: 2,
+                                    borderColor:
+                                        selectedRole === "patient"
+                                            ? "#3b82f6"
+                                            : "#f8f8f8",
+                                    borderRadius: 16,
+                                    flexDirection: "row",
+                                    overflow: "hidden",
+                                }}
                             >
-                                <Trans>Next</Trans>
-                            </Text>
+                                <View
+                                    className="items-center"
+                                    style={{
+                                        flex: 1,
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        paddingHorizontal: 12,
+                                    }}
+                                >
+                                    <View
+                                        style={{
+                                            flex: 1,
+                                        }}
+                                    >
+                                        <Text className="font-nunito-sans text-2xl">
+                                            <Trans>Patient</Trans>
+                                        </Text>
+                                        <Text className="font-nunito-sans text-slate-500">
+                                            <Trans>
+                                                Looking for help or just want to
+                                                talk
+                                            </Trans>
+                                        </Text>
+                                    </View>
+                                    <Image
+                                        alt="Patient with a plant"
+                                        source={require("../../assets/images/girl_flower.png")}
+                                        style={{
+                                            maxWidth: 180,
+                                            maxHeight: 180,
+                                            position: "relative",
+                                            bottom: -24,
+                                        }}
+                                        resizeMode="contain"
+                                    />
+                                </View>
+                            </Pressable>
                         </View>
-                    </TouchableOpacity>
+                        <View
+                            style={{
+                                flex: 1,
+                                maxHeight: "30%",
+                            }}
+                        >
+                            <Pressable
+                                onPress={() => setSelectedRole("professional")}
+                                style={{
+                                    flex: 1,
+                                    opacity: setMetadata.isLoading ? 0.3 : 1,
+                                    borderWidth: 2,
+                                    borderColor:
+                                        selectedRole === "professional"
+                                            ? "#3b82f6"
+                                            : "#f8f8f8",
+                                    borderRadius: 16,
+                                    flexDirection: "row",
+                                    overflow: "hidden",
+                                }}
+                            >
+                                <View
+                                    className="items-center"
+                                    style={{
+                                        flex: 1,
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        paddingHorizontal: 12,
+                                    }}
+                                >
+                                    <Image
+                                        alt="Patient with a plant"
+                                        source={require("../../assets/images/girl_book.png")}
+                                        style={{
+                                            // for some reason it looks better if this image if smaller
+                                            maxWidth: 160,
+                                            maxHeight: 160,
+                                            position: "relative",
+                                            bottom: -24,
+                                        }}
+                                        resizeMode="contain"
+                                    />
+                                    <View
+                                        style={{
+                                            flex: 1,
+                                        }}
+                                    >
+                                        <Text className="font-nunito-sans text-2xl">
+                                            <Trans>Professional</Trans>
+                                        </Text>
+                                        <Text className="font-nunito-sans text-slate-500">
+                                            <Trans>
+                                                Meet and help new patients
+                                            </Trans>
+                                        </Text>
+                                    </View>
+                                </View>
+                            </Pressable>
+                        </View>
+                    </View>
                 </View>
+
+                <LargeButton
+                    loading={loading}
+                    onPress={handleNext}
+                    disabled={!selectedRole}
+                >
+                    <Trans>Next</Trans>
+                </LargeButton>
             </View>
-        </SafeAreaView>
+        </ScreenWrapper>
     );
 }
 
