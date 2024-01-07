ba41dd98db8b56766597dd677c3dcc424f935d4d
Abdul Haidar <abdulhdr1@gmail.com>
2024-01-07 01:02:29 -0300

    add color prop to LargeButto

--- apps/expo/src/components/LargeButton.tsx
+++ apps/expo/src/components/LargeButton.tsx
@@ -4,21 +4,43 @@ import { BasicText } from "./BasicText";
 import { Loading } from "./Loading";
 
 type TouchableOpacityProps = ConstructorParameters<typeof TouchableOpacity>[0];
+type ButtonColors = "red" | "blue";
+
+function getButtonColor({
+    color,
+    disabled = false,
+}: {
+    color: ButtonColors;
+    disabled?: boolean;
+}) {
+    switch (color) {
+        case "red":
+            if (disabled) return "#F87171";
+            return "#EF4444";
+        case "blue":
+            if (disabled) return "#BFDBFE";
+            return "#3B82F6";
+    }
+}
 
 export function LargeButton({
     children,
+    color = "blue",
     ...props
 }: TouchableOpacityProps & {
     loading?: boolean;
     children: React.ReactNode;
     disabled?: boolean;
+    color?: ButtonColors;
 }) {
     return (
         <TouchableOpacity
             style={{
                 width: "100%",
-                backgroundColor:
-                    props.disabled || props.loading ? "#bfdbfe" : "#3b82f6",
+                backgroundColor: getButtonColor({
+                    color,
+                    disabled: props.disabled || props.loading,
+                }),
                 borderRadius: 10,
                 paddingVertical: 8,
                 alignItems: "center",
