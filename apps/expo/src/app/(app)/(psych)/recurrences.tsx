6704ced9e94ae98825f5b92a7fc35f1278210052
Abdul Haidar <abdulhdr1@gmail.com>
2024-01-15 15:39:29 -0300

    add recurrences screen

---
+++ apps/expo/src/app/(app)/(psych)/recurrences.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../../components/Header";
+import { ScreenWrapper } from "../../../components/ScreenWrapper";
+
+export default function RecurrencesScreen() {
+    return (
+        <ScreenWrapper>
+            <Header />
+        </ScreenWrapper>
+    );
+}
