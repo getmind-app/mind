2b4071cda0c09c579dac701f769153316bbbc673
Abdul Haidar <abdulhdr1@gmail.com>
2024-01-16 14:56:26 -0300

    remove unused variables

--- apps/expo/src/app/(app)/settings/personal-info.tsx
+++ apps/expo/src/app/(app)/settings/personal-info.tsx
@@ -62,7 +62,7 @@ function TherapistOptions() {
     const {
         control,
         handleSubmit,
-        formState: { isValid, isDirty, isSubmitting },
+        formState: { isSubmitting },
     } = useForm<NonNullable<typeof therapist.data>>({
         defaultValues: therapist.data
             ? {
