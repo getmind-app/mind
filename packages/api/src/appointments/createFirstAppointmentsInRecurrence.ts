701c0fb0229062bdc52074e0859c1ecd0236846e
Abdul Haidar <abdulhdr1@gmail.com>
2024-01-17 17:42:11 -0300

    refactor createFirstAppointmentsInRecurrence

--- packages/api/src/appointments/createFirstAppointmentsInRecurrence.ts
+++ packages/api/src/appointments/createFirstAppointmentsInRecurrence.ts
@@ -1,19 +1,37 @@
 import { type inferAsyncReturnType } from "@trpc/server";
+import { addDays, addWeeks, differenceInWeeks } from "date-fns";
 
 import { type createContext } from "../trpc";
 
 export async function createFirstAppointmentsInRecurrence({
-    auth,
     prisma,
     recurrenceId,
 }: inferAsyncReturnType<typeof createContext> & {
     recurrenceId: string;
 }) {
-    const firstAppointment = await prisma.appointment.findFirstOrThrow({
+    const recurrence = await prisma.recurrence.findUniqueOrThrow({
         where: {
-            recurrenceId,
+            id: recurrenceId,
         },
     });
 
-    firstAppointment.scheduledTo;
+    const firstDate = recurrence.startAt;
+    const nextMonth = addDays(new Date(), 31);
+    const weeksToSchedule = differenceInWeeks(nextMonth, firstDate);
+
+    for (let i = 0; i < weeksToSchedule; i++) {
+        const date = addWeeks(firstDate, 1);
+
+        await prisma.appointment.create({
+            data: {
+                recurrenceId,
+                modality: recurrence.defaultModality,
+                scheduledTo: date,
+                patientId: recurrence.patientId,
+                therapistId: recurrence.therapistId,
+                status: "ACCEPTED",
+                type: "RECURRENT",
+            },
+        });
+    }
 }
