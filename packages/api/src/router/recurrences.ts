f851a975c4e081312a6a1ec364692cf65fe3621f
Abdul Haidar <abdulhdr1@gmail.com>
2024-01-17 20:13:40 -0300

    add checkRecurrenceCanHappen to recurrenceRouter

--- packages/api/src/router/recurrences.ts
+++ packages/api/src/router/recurrences.ts
@@ -1,50 +1,21 @@
 import { z } from "zod";
 
+import { checkForRecurrenceConflict } from "../appointments/checkForRecurrenceConflict";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
-export const notesRouter = createTRPCRouter({
-    create: protectedProcedure
+export const recurrenceRouter = createTRPCRouter({
+    checkRecurrenceCanHappen: protectedProcedure
         .input(
             z.object({
-                content: z.string().min(1),
-            }),
-        )
-        .mutation(async ({ ctx, input }) => {
-            return await ctx.prisma.note.create({
-                data: {
-                    ...input,
-                    userId: ctx.auth.userId,
-                },
-            });
-        }),
-    findByUserId: protectedProcedure.query(async ({ ctx }) => {
-        const notes = await ctx.prisma.note.findMany({
-            where: { userId: ctx.auth.userId },
-            orderBy: {
-                createdAt: "desc",
-            },
-        });
-
-        return notes;
-    }),
-    delete: protectedProcedure
-        .input(
-            z.object({
-                id: z.string().min(1),
-            }),
-        )
-        .mutation(async ({ ctx, input }) => {
-            return await ctx.prisma.note.delete({ where: { id: input.id } });
-        }),
-    findById: protectedProcedure
-        .input(
-            z.object({
-                id: z.string().min(1),
+                therapistId: z.string(),
+                scheduledTo: z.date(),
             }),
         )
         .query(async ({ ctx, input }) => {
-            return await ctx.prisma.note.findUnique({
-                where: { id: input.id },
-            });
+            return !(await checkForRecurrenceConflict({
+                prisma: ctx.prisma,
+                therapistId: input.therapistId,
+                scheduledTo: input.scheduledTo,
+            }));
         }),
 });
