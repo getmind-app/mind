f104a297ff4c4df3e87c4e17756c810718a44183
Gustavo <gustavo_fior@outlook.com>
2024-01-31 22:03:16 -0300

    increase version

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -25,7 +25,7 @@ const defineConfig = (): ExpoConfig => ({
     name: "Mind",
     slug: "mind",
     scheme: "mind",
-    version: "2.1.3",
+    version: "2.1.4",
     orientation: "portrait",
     owner: "mind-therapy",
     icon: "./assets/icon.png",
@@ -40,7 +40,7 @@ const defineConfig = (): ExpoConfig => ({
     },
     assetBundlePatterns: ["**/*", "assets/*"],
     ios: {
-        buildNumber: "18",
+        buildNumber: "20",
         supportsTablet: true,
         bundleIdentifier: "app.getmind",
         infoPlist: {
