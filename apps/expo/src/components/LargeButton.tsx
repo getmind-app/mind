df634fd97b9c93d258e273109234a74a11408e8e
Abdul Haidar <abdulhdr1@gmail.com>
2024-02-16 15:03:59 -0300

    refactor LargeButton to use colors

--- apps/expo/src/components/LargeButton.tsx
+++ apps/expo/src/components/LargeButton.tsx
@@ -1,51 +1,35 @@
 import { TouchableOpacity, View } from "react-native";
 
+import { colors, disabledColor, type Color } from "../utils/colors";
 import { BasicText } from "./BasicText";
 import { Loading } from "./Loading";
 
 type TouchableOpacityProps = ConstructorParameters<typeof TouchableOpacity>[0];
-type ButtonColors = "red" | "blue";
-
-function getButtonColor({
-    color,
-    disabled = false,
-}: {
-    color: ButtonColors;
-    disabled?: boolean;
-}) {
-    switch (color) {
-        case "red":
-            if (disabled) return "#F87171";
-            return "#EF4444";
-        case "blue":
-            if (disabled) return "#BFDBFE";
-            return "#3B82F6";
-    }
-}
 
 export function LargeButton({
     children,
-    color = "blue",
+    color = "primaryBlue",
+    disabled = false,
     ...props
 }: TouchableOpacityProps & {
     loading?: boolean;
     children: React.ReactNode;
     disabled?: boolean;
-    color?: ButtonColors;
+    color?: Color;
 }) {
     return (
         <TouchableOpacity
             style={{
                 width: "100%",
-                backgroundColor: getButtonColor({
-                    color,
-                    disabled: props.disabled || props.loading,
-                }),
+                backgroundColor: disabled
+                    ? disabledColor(colors[color])
+                    : colors[color],
                 borderRadius: 10,
                 paddingVertical: 12,
                 alignItems: "center",
                 justifyContent: "center",
             }}
+            disabled={disabled}
             {...props}
         >
             <View
