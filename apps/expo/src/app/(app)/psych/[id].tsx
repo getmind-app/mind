da2d25482eb45a90fdda23b2826f023687495237
Gustavo <gustavo_fior@outlook.com>
2024-02-16 23:16:34 -0300

    impl lock map on therapist profile and address screens

--- apps/expo/src/app/(app)/psych/[id].tsx
+++ apps/expo/src/app/(app)/psych/[id].tsx
@@ -340,6 +340,7 @@ function LocationContent({ address }: { address: Address }) {
                             altitude: 1000,
                             zoom: 15,
                         }}
+                        scrollEnabled={false}
                     >
                         <Marker
                             // TODO: remove this
