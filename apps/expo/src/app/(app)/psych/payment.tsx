65458ee5f7dd4fb0b4d0f893e4bbe69940ed2c4e
Abdul Haidar <abdulhdr1@gmail.com>
2024-02-18 21:17:58 -0300

    comment out unused return in psych/payment

--- apps/expo/src/app/(app)/psych/payment.tsx
+++ apps/expo/src/app/(app)/psych/payment.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import { Alert, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Alert, ScrollView, Text, View } from "react-native";
 import * as Linking from "expo-linking";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { Trans, t } from "@lingui/macro";
+import { t } from "@lingui/macro";
 import { usePaymentSheet } from "@stripe/stripe-react-native";
 
 import { Header } from "../../../components/Header";
 import { ProfileSkeleton } from "../../../components/ProfileSkeleton";
-import { ScreenWrapper } from "../../../components/ScreenWrapper";
 import { api } from "../../../utils/api";
 
 function handleMode(x: string) {
@@ -109,81 +108,81 @@ export default function AppointmentPaymentScreen() {
 
     return <Text>This is currently disabled</Text>;
 
-    return (
-        <>
-            <Header />
-            <ScreenWrapper>
-                <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    overScrollMode="never"
-                >
-                    <View className="rounded-2xl bg-white px-4 py-6 shadow-sm">
-                        <Text className="font-nunito-sans-bold text-2xl">
-                            <Trans>Your appointment</Trans>
-                        </Text>
-                        <Text className="pl-0.5 pt-2 font-nunito-sans text-sm">
-                            <Trans>Details</Trans>
-                        </Text>
-                        <Text className="pl-3 pt-2 font-nunito-sans text-[#666666]">
-                            <Trans>
-                                Appointments with {data?.therapist.name}
-                            </Trans>
-                        </Text>
-                        <View>
-                            <View className="flex flex-row justify-between pl-6 pt-2">
-                                <Text className="font-nunito-sans text-[#666666]">
-                                    {data?.scheduledTo.getDate()} -{" "}
-                                    {data?.scheduledTo.getHours()}:
-                                    {data?.scheduledTo.getMinutes() == 0
-                                        ? "00"
-                                        : data?.scheduledTo.getMinutes()}{" "}
-                                    -{" "}
-                                    {handleMode(
-                                        data?.modality ? data?.modality : "",
-                                    )}
-                                </Text>
-                                <Text className=" font-nunito-sans text-[#666666]">
-                                    {"R$ "}
-                                    {data?.therapist.hourlyRate}
-                                </Text>
-                            </View>
-                        </View>
-                        <View className="flex flex-row justify-between pt-4">
-                            <Text className="pl-0.5 font-nunito-sans">
-                                <Trans>Total</Trans>
-                            </Text>
-                            <Text className="font-nunito-sans">
-                                {data?.therapist && data?.therapist.hourlyRate
-                                    ? "R$ " + data.therapist.hourlyRate
-                                    : "N/A"}
-                            </Text>
-                        </View>
-                        <View>
-                            <Text className="pt-8 font-nunito-sans-bold text-2xl">
-                                <Trans>Payment method</Trans>
-                            </Text>
+    // return (
+    //     <>
+    //         <Header />
+    //         <ScreenWrapper>
+    //             <ScrollView
+    //                 showsVerticalScrollIndicator={false}
+    //                 overScrollMode="never"
+    //             >
+    //                 <View className="rounded-2xl bg-white px-4 py-6 shadow-sm">
+    //                     <Text className="font-nunito-sans-bold text-2xl">
+    //                         <Trans>Your appointment</Trans>
+    //                     </Text>
+    //                     <Text className="pl-0.5 pt-2 font-nunito-sans text-sm">
+    //                         <Trans>Details</Trans>
+    //                     </Text>
+    //                     <Text className="pl-3 pt-2 font-nunito-sans text-[#666666]">
+    //                         <Trans>
+    //                             Appointments with {data?.therapist.name}
+    //                         </Trans>
+    //                     </Text>
+    //                     <View>
+    //                         <View className="flex flex-row justify-between pl-6 pt-2">
+    //                             <Text className="font-nunito-sans text-[#666666]">
+    //                                 {data?.scheduledTo.getDate()} -{" "}
+    //                                 {data?.scheduledTo.getHours()}:
+    //                                 {data?.scheduledTo.getMinutes() == 0
+    //                                     ? "00"
+    //                                     : data?.scheduledTo.getMinutes()}{" "}
+    //                                 -{" "}
+    //                                 {handleMode(
+    //                                     data?.modality ? data?.modality : "",
+    //                                 )}
+    //                             </Text>
+    //                             <Text className=" font-nunito-sans text-[#666666]">
+    //                                 {"R$ "}
+    //                                 {data?.therapist.hourlyRate}
+    //                             </Text>
+    //                         </View>
+    //                     </View>
+    //                     <View className="flex flex-row justify-between pt-4">
+    //                         <Text className="pl-0.5 font-nunito-sans">
+    //                             <Trans>Total</Trans>
+    //                         </Text>
+    //                         <Text className="font-nunito-sans">
+    //                             {data?.therapist && data?.therapist.hourlyRate
+    //                                 ? "R$ " + data.therapist.hourlyRate
+    //                                 : "N/A"}
+    //                         </Text>
+    //                     </View>
+    //                     <View>
+    //                         <Text className="pt-8 font-nunito-sans-bold text-2xl">
+    //                             <Trans>Payment method</Trans>
+    //                         </Text>
 
-                            <TouchableOpacity
-                                disabled={loadingPaymentSheet || !ready}
-                                onPress={handleConfirm}
-                                className={`${
-                                    loadingPaymentSheet
-                                        ? "bg-blue-300"
-                                        : "bg-blue-500"
-                                } mt-2 rounded-lg py-2`}
-                            >
-                                <Text
-                                    className={
-                                        "text-center font-nunito-sans-bold text-base text-white"
-                                    }
-                                >
-                                    <Trans>Confirm</Trans>
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </ScrollView>
-            </ScreenWrapper>
-        </>
-    );
+    //                         <TouchableOpacity
+    //                             disabled={loadingPaymentSheet || !ready}
+    //                             onPress={handleConfirm}
+    //                             className={`${
+    //                                 loadingPaymentSheet
+    //                                     ? "bg-blue-300"
+    //                                     : "bg-blue-500"
+    //                             } mt-2 rounded-lg py-2`}
+    //                         >
+    //                             <Text
+    //                                 className={
+    //                                     "text-center font-nunito-sans-bold text-base text-white"
+    //                                 }
+    //                             >
+    //                                 <Trans>Confirm</Trans>
+    //                             </Text>
+    //                         </TouchableOpacity>
+    //                     </View>
+    //                 </View>
+    //             </ScrollView>
+    //         </ScreenWrapper>
+    //     </>
+    // );
 }
