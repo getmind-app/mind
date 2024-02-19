f6ec8e12c7e88969035d275706e75eceee3eabf4
Abdul Haidar <abdulhdr1@gmail.com>
2024-02-18 21:57:18 -0300

    add CopyPixKeyButton to AppointmentCard

--- apps/expo/src/app/(app)/calendar.tsx
+++ apps/expo/src/app/(app)/calendar.tsx
@@ -27,6 +27,7 @@ import { enUS, ptBR } from "date-fns/locale";
 import { BasicText } from "../../components/BasicText";
 import { Card } from "../../components/Card";
 import { CardSkeleton } from "../../components/CardSkeleton";
+import { CopyButton } from "../../components/CopyButton";
 import { LargeButton } from "../../components/LargeButton";
 import { Refreshable } from "../../components/Refreshable";
 import { ScreenWrapper } from "../../components/ScreenWrapper";
@@ -290,9 +291,10 @@ function AppointmentCard({
                     ) : null}
                 </View>
             </View>
+            {!isProfessional && <CopyPixKeyButton appointment={appointment} />}
             {open ? (
                 <View className="mt-4 border-t-[1px] border-slate-500/10">
-                    {metadata.role == "professional" ? (
+                    {isProfessional ? (
                         <TherapistOptions appointment={appointment} />
                     ) : (
                         <PatientOptions appointment={appointment} />
@@ -675,5 +677,19 @@ function UserPhoto({
                 height: 20,
             }}
         />
+    );
+}
+
+function CopyPixKeyButton({
+    appointment,
+}: {
+    appointment: Appointment & { therapist: Therapist };
+}) {
+    if (!appointment.therapist.pixKey) return null;
+
+    return (
+        <CopyButton stringToCopy={appointment.therapist.pixKey}>
+            <Trans>Copy pix key</Trans>
+        </CopyButton>
     );
 }
