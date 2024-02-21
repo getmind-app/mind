baa3e490d61d3e8a3cda5e367334288b2ba01eba
Abdul Haidar <abdulhdr1@gmail.com>
2024-02-21 15:56:15 -0300

    pass style prop to TouchableTag

--- apps/expo/src/components/TouchableTag.tsx
+++ apps/expo/src/components/TouchableTag.tsx
@@ -7,6 +7,7 @@ import {
 export function TouchableTag({
     checked,
     children,
+    style,
     ...rest
 }: {
     checked: boolean;
@@ -20,6 +21,7 @@ export function TouchableTag({
                 borderWidth: 1,
                 borderColor: checked ? "#3b82f6" : "#64748b",
                 elevation: 2,
+                ...(style as object),
             }}
             {...rest}
         >
